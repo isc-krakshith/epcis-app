37461674e4b13773d7a0601a26b4174f310a8f92
5RN <kinshuk.rakshith@intersystems.com>
2021-07-01 18:07:53 +0100

    refactored and added functionality to serve runner-app

--- src/app/services/epcis-iris.service.ts
+++ src/app/services/epcis-iris.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable, of } from 'rxjs';
+import { Observable, of, throwError } from 'rxjs';
 import { catchError, map, tap } from 'rxjs/operators';
 import { MessageService } from './message.service';
 
@@ -37,11 +37,22 @@ export class EPCISIRISService {
       'responseType': 'json'})
   };
 
+  httpBlobOptions = {
+    headers: new HttpHeaders({ 
+      'Content-Type': 'application/json',
+      'responseType': 'blob'})
+  };
 
   httpTextOptions = {
     headers: new HttpHeaders({ 
       'Content-Type': 'application/json',
       'responseType': 'text'})
+  };
+
+  httpArrayBufferOptions = {
+    headers: new HttpHeaders({ 
+    'Content-Type': 'application/json',
+    'responseType': 'arraybuffer'})
   };
 
   admitPatient(admitForm: any):Observable<any> {
@@ -50,18 +61,10 @@ export class EPCISIRISService {
     let url = 'http://localhost:52773/query/demoStep1'
     return this.http.post<any>(url, admitForm,this.httpOptions)
     .pipe(
-      tap((newAdmit: any) => this.log(`${newAdmit.PAS}`)),
+      tap((newAdmit: any) => this.log('Admit: ' + `${newAdmit.PAS}`)),
       catchError(this.handleError<any>('admitPatient')))
   }
-  dischargePatient(admitForm: any):Observable<any> {
 
-    const headers = new HttpHeaders()
-    let url = 'http://localhost:52773/query/demoStep8'
-    return this.http.post<any>(url, admitForm,this.httpOptions)
-    .pipe(
-      tap((newDischarge: any) => this.log(`${newDischarge.PAS}`)),
-      catchError(this.handleError<any>('dischargePatient')))
-  }
   linkDischarge():Observable<any> {
     const headers = new HttpHeaders()
     let url = 'http://localhost:52773/query/demoStep2'
@@ -71,15 +74,6 @@ export class EPCISIRISService {
       catchError(this.handleError<any>('linkDischarge')))
   }
 
-  linkPigeonHole():Observable<any> {
-    const headers = new HttpHeaders()
-    let url = 'http://localhost:52773/query/demoStep5'
-    return this.http.get<any>(url)
-    .pipe(
-      tap((newLink: any) => this.log('Link Pigeon Hole : '+`${newLink}`)),
-      catchError(this.handleError<any>('linkPigeonhole')))
-  }
-  
   getInpatientSpellId(genLabelForm: any):Observable<any> {
     const headers = new HttpHeaders()
     let url = 'http://localhost:52773/query/demoStep3'
@@ -98,6 +92,55 @@ export class EPCISIRISService {
       tap((any: any) => this.log('Patient Location Id: '+`${any.EventQueryResult[0].EPCISBody.EventList.ObjectEvent[0].bizLocation.id}`)),
       catchError(this.handleError<any>('getPatientLocationId')))
 
+  }
+
+  linkPigeonHole():Observable<any> {
+    const headers = new HttpHeaders()
+    let url = 'http://localhost:52773/query/demoStep5'
+    return this.http.get<any>(url)
+    .pipe(
+      tap((newLink: any) => this.log('Link Pigeon Hole : '+`${newLink}`)),
+      catchError(this.handleError<any>('linkPigeonhole')))
+  }
+
+  idetifyPigeonHole(testPayload:any):Observable<any> {
+    const headers = new HttpHeaders()
+    let url = 'http://localhost:52773/query/demoStep6'
+    return this.http.get<any>(url,this.httpTextOptions)
+    .pipe(
+      tap((any: any) => this.log('Identify Pigeon Hole : '+`${any}`)),
+      //Sorry this is a hack... Complete REST response can be seen in the REST CALL LOGS TAB
+      //but it cannot be parsed and an http parsing error is reported.
+      //Below we are effectively making the error OK :-|
+      catchError((any) => {
+        this.log('Identify Pigeon Hole : ' /*'+ `${any.error.error}` + " Response follows... : '*/ + `${any.error.text}`);
+        return throwError(any);
+      }))
+  }
+
+  retrieveItems(retrieveForm: any):Observable<any> {
+    const headers = new HttpHeaders()
+    let url = 'http://localhost:52773/query/demoStep7'
+    return this.http.get<any>(url,retrieveForm)
+    .pipe(
+      tap((any: any) => this.log('Retrieve Items : '+`${any}`)),
+      //Sorry this is a hack... Complete REST response can be seen in the REST CALL LOGS TAB
+      //but it cannot be parsed and an http parsing error is reported.
+      //Below we are effectively making the error OK :-|
+      catchError((any) => {
+        this.log('Retrieve Items : ' /*'+ `${any.error.error}` + " Response follows... : '*/ + `${any.error.text}`);
+        return throwError(any);
+      }))
+  }
+
+  dischargePatient(admitForm: any):Observable<any> {
+
+    const headers = new HttpHeaders()
+    let url = 'http://localhost:52773/query/demoStep8'
+    return this.http.post<any>(url, admitForm,this.httpOptions)
+    .pipe(
+      tap((newDischarge: any) => this.log('Discharge: ' + `${newDischarge.PAS}`)),
+      catchError(this.handleError<any>('dischargePatient')))
   }
 
 }
