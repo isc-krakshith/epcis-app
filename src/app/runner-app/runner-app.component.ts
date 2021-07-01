4173fe2a4f44bbbc0b927f3aa82c68e99885f59f
5RN <kinshuk.rakshith@intersystems.com>
2021-07-01 18:05:49 +0100

    added functionality

--- src/app/runner-app/runner-app.component.ts
+++ src/app/runner-app/runner-app.component.ts
@@ -8,8 +8,39 @@ import { Component, OnInit } from '@angular/core';
 export class RunnerAppComponent implements OnInit {
 
   constructor() { }
+  isIdentifyPigeonHole = false;
+  isIdentifyWard = false;
+  isRetrieveItems = false;
+  isComingSoon = false;
 
   ngOnInit(): void {
   }
 
+  loadContent(selection: String)
+  {
+    if (selection == 'identifyPigeonHole'){
+      this.isComingSoon = false;
+      this.isIdentifyPigeonHole=true;
+      this.isIdentifyWard = false;
+      this.isRetrieveItems = false;
+    }
+    else if (selection == 'identifyWard'){
+      this.isIdentifyPigeonHole=false;
+      this.isIdentifyWard = true;
+      this.isComingSoon = false;
+      this.isRetrieveItems = false;
+    }
+    else if (selection == 'retrieveItems'){
+      this.isIdentifyPigeonHole=false;
+      this.isIdentifyWard = false;
+      this.isComingSoon = false;
+      this.isRetrieveItems = true;
+    }
+    else if (selection == 'comingsoon'){
+      this.isIdentifyPigeonHole=false;
+      this.isIdentifyWard = false;
+      this.isComingSoon = true;
+      this.isRetrieveItems = false;
+    }
+  }
 }
